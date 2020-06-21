8d3a5b5102d8a5c6cf31030a2b017385f706ea6e
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2020-06-22 05:50:32 +0900

    Django 프로젝트 생성

---
+++ SplitCalc/transactionApp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Transaction(models.model):
+    datetime = models.Datetime()
+    pic = models.
+    price = models.
+    # friends = 
